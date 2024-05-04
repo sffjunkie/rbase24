7f7e1034deefc9fd2bbbb7ad31b064e57cb90238
Simon Kennedy <sffjunkie+code@gmail.com>
2024-05-04 11:56:27 +0100

    Add pytest marks

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,29 +1,35 @@
 from pathlib import Path
 
+import pytest
 from rbase24.config import Base24ViewerConfig
 
 
+@pytest.mark.unit
 def test_config_default_scheme_dir():
     sd = Base24ViewerConfig.default_scheme_dir()
     assert sd == Path("~/.local/share/tinted-themeing/schemes").expanduser()
 
 
+@pytest.mark.unit
 def test_config_default_config_path():
     cp = Base24ViewerConfig.default_config_path()
     assert cp == Path("~/.config/rbase24/config.ini").expanduser()
 
 
+@pytest.mark.unit
 def test_config_default_config():
     dc = Base24ViewerConfig.default_config()
     assert dc == {"scheme_dir": Base24ViewerConfig.default_scheme_dir()}
 
 
+@pytest.mark.unit
 def test_config_local():
     cf = Path(__file__).parent / "config.ini"
     cfg = Base24ViewerConfig(cf)
     assert cfg.scheme_dir.name == "schemes"
 
 
+@pytest.mark.unit
 def test_config_bad_path():
     badcfg = Path("notthere")
     cfg = Base24ViewerConfig(badcfg)
--- tests/test_scheme.py
+++ tests/test_scheme.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
-from rbase24.scheme import load_scheme, list_schemes
+import pytest
 from rbase24.config import Base24ViewerConfig
+from rbase24.scheme import list_schemes, load_scheme
 
 
+@pytest.mark.unit
 def test_scheme_load_base16_scheme():
     cf = Path(__file__).parent / "config.ini"
     cfg = Base24ViewerConfig(cf)
@@ -13,6 +15,7 @@ def test_scheme_load_base16_scheme():
     assert "fdf0ed" == scheme["palette"]["base00"]
 
 
+@pytest.mark.unit
 def test_scheme_load_base24_scheme():
     cf = Path(__file__).parent / "config.ini"
     cfg = Base24ViewerConfig(cf)
@@ -22,10 +25,11 @@ def test_scheme_load_base24_scheme():
     assert "524fb9" == scheme["palette"]["base17"]
 
 
+@pytest.mark.unit
 def test_scheme_list_schemes():
     cf = Path(__file__).parent / "config.ini"
     cfg = Base24ViewerConfig(cf)
     scheme_dir = cfg.scheme_dir
     scheme_dir = Path(__file__).parent / scheme_dir
 
-    assert 9 == len(list_schemes(scheme_dir))
+    assert 10 == len(list_schemes(scheme_dir))
