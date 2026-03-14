6c1ebc9629977e44574c78d1b345acc2992a7ed7
Simon Kennedy <sffjunkie+code@gmail.com>
2026-03-14 17:58:34 +0000

    Switch to standard flake template

--- flake.nix
+++ flake.nix
@@ -32,54 +32,56 @@
       ...
     }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
+      inherit (nixpkgs) lib;
+
+      forAllSystems = lib.genAttrs [
         "x86_64-linux"
       ];
 
-      inherit (nixpkgs) lib;
+      pyproject = pyproject-nix.lib.project.loadPyproject {
+        projectRoot = ./.;
+      };
 
-      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+      workspace = uv2nix.lib.workspace.loadWorkspace {
+        workspaceRoot = ./.;
+      };
 
       overlay = workspace.mkPyprojectOverlay {
         sourcePreference = "wheel";
       };
 
-      python = pkgs.python312;
-      pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+      editableOverlay = workspace.mkEditablePyprojectOverlay {
+        root = "$REPO_ROOT";
+      };
 
-      pythonSet =
-        # Use base package set from pyproject.nix builders
+      pythonSets = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python3;
+        in
         (pkgs.callPackage pyproject-nix.build.packages {
           inherit python;
         }).overrideScope
           (
             lib.composeManyExtensions [
-              pyproject-build-systems.overlays.default
+              pyproject-build-systems.overlays.wheel
               overlay
             ]
-          );
+          )
+      );
     in
     {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pythonSet.mkVirtualEnv "rbase24-env" workspace.deps.default;
-        }
-      );
+      packages = forAllSystems (system: {
+        default =
+          pythonSets.${system}.mkVirtualEnv "${pyproject.pyproject.project.name}-env"
+            workspace.deps.default;
+      });
 
       apps = forAllSystems (system: {
         default = {
           type = "app";
-          program = "${self.packages.${system}.default}/bin/rbase24";
-          meta = {
-            description = "CLI Base16 ciolor scheme viewer";
-            homepage = "https://github.com/sffjunkie/rbase24";
-            license = lib.licenses.asl20;
-          };
+          program = "${self.packages.${system}.default}/bin/${pyproject.pyproject.project.name}";
         };
       });
 
@@ -87,30 +89,28 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python3;
+          pythonSet = pythonSets.${system}.overrideScope editableOverlay;
+          virtualenv = pythonSet.mkVirtualEnv "${pyproject.pyproject.project.name}-env" workspace.deps.all;
         in
         {
+
           default = pkgs.mkShell {
             packages = [
-              python
-              pkgs.uv
-              pkgs.ty
+              virtualenv
               pkgs.ruff
-
-              self.packages.${system}.default
+              pkgs.just
+              pkgs.pre-commit
+              pkgs.uv
             ];
             env = {
-              # Prevent uv from managing Python downloads
+              NIX_DEVSHELL_PROJECT = pyproject.pyproject.project.name;
               UV_PYTHON_DOWNLOADS = "never";
-              # Force uv to use nixpkgs Python interpreter
               UV_PYTHON = python.interpreter;
-            }
-            // lib.optionalAttrs pkgs.stdenv.isLinux {
-              # Python libraries often load native shared objects using dlopen(3).
-              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
             };
             shellHook = ''
               unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
             '';
           };
         }
